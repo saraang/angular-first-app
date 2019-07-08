a13e97055e6d6a17c8993e84fad1f9383ee884d1
saraang <saraangsk@gmail.com>
2019-07-08 12:44:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3c5dvd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3c5dvd)
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+
+import { AppModule } from './app/app.module';
+
+import './polyfills';
+
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
